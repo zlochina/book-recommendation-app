f5c4fece02638dfc92a5dfc715624988640f48fb
Vladyslav Zlochevskyi <zlochvla@fel.cvut.cz>
2025-08-22 17:22:50 +0200

    Added todo to main.py

--- src/book_recommendation_prod/main.py
+++ src/book_recommendation_prod/main.py
@@ -6,6 +6,7 @@ app = FastAPI(title="Book Recommendation API")
 # Include routers
 app.include_router(book_user.router, prefix="/books", tags=["Books"])
 app.include_router(rating_user.router, prefix="/ratings", tags=["Ratings"])
+# TODO: add maintainer api
 
 @app.get("/")
 def root():
